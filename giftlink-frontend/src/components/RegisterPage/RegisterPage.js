2136d6a446a9c9b4b60f757fafd312a494653bc4
jberrocal2016 <jberrocal2016@fau.edu>
2025-11-10 22:11:22 -0500

    register component inplemented

--- giftlink-frontend/src/components/RegisterPage/RegisterPage.js
+++ giftlink-frontend/src/components/RegisterPage/RegisterPage.js
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import {urlConfig} from '../../config';
+import { useAppContext } from '../../context/AuthContext';
+import { useNavigate } from 'react-router-dom';
 
 import './RegisterPage.css';
 
@@ -9,9 +12,45 @@ function RegisterPage() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
+    const [showerr, setShowerr] = useState('');
+    const navigate = useNavigate();
+    const { setIsLoggedIn } = useAppContext();
+
     // insert code here to create handleRegister function and include console.log
     const handleRegister = async () => {
-        console.log("Register invoked")
+        const response = await fetch(`${urlConfig.backendUrl}/api/auth/register`, {
+                //Step 1 - Task 6
+                method: 'POST',
+                //Step 1 - Task 7
+                headers: {
+                    'content-type': 'application/json',
+                },
+                //Step 1 - Task 8
+                body: JSON.stringify({
+                    firstName: firstName,
+                    lastName: lastName,
+                    email: email,
+                    password: password
+                })
+            });
+            //Step 2 - Task 1
+            const json = await response.json();
+            console.log('json data', json);
+            console.log('er', json.error);
+            //Step 2 - Task 2
+            if (json.authtoken) {
+                sessionStorage.setItem('auth-token', json.authtoken);
+                sessionStorage.setItem('name', firstName);
+                sessionStorage.setItem('email', json.email);
+            //Step 2 - Task 3
+                setIsLoggedIn(true);
+            //Step 2 - Task 4
+                navigate('/app');
+            }
+            if (json.error) {
+            //Step 2 - Task 5
+                setShowerr(json.error);
+            }
     }
 
     return (
@@ -60,6 +99,7 @@ function RegisterPage() {
                                 value={email}
                                 onChange={(e) => setEmail(e.target.value)}
                             />
+                            <div className="text-danger">{showerr}</div>
                         </div>
 
                         <div className="mb-4">
