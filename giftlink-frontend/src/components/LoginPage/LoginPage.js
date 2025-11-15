d865ecfb45c7d7be5150c661334d7c500c79f84b
jberrocal2016 <jberrocal2016@fau.edu>
2025-11-14 23:13:10 -0500

    adding Link import

--- giftlink-frontend/src/components/LoginPage/LoginPage.js
+++ giftlink-frontend/src/components/LoginPage/LoginPage.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { urlConfig } from "../../config";
 import { useAuthContext } from "../../context/AuthContext";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import "./LoginPage.css";
 
 function LoginPage() {
